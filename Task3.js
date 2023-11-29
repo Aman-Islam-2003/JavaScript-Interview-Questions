cb4f62758e7596102feaaeb452850d65bf2f870e
Aman-Islam-2003 <amanislam019@gmail.com>
2023-11-29 23:49:31 +0500

    Create Task3.js

---
+++ Task3.js
@@ -0,0 +1,18 @@
+// Reverse Words in a String
+// Write a function reverseWords that takes a string as an input and returns the same string with each word reversed, while maintaining the order of the words in the original string.
+
+let string1 = "Hello World";
+
+const reverseWords = (str)=>{
+   let strArray = str.trim().split(/\s+/);
+   //console.log(strArray);
+   console.log(strArray.reverse());
+}
+
+
+reverseWords(string1); // Output: "olleH dlroW"
+// console.log(reverseWords("The quick brown fox")); // Output: "ehT kciuq nworb xof"
+
+// Words are separated by a single space.
+// You can assume that the input string does not have leading or trailing spaces.
+// The function should handle consecutive spaces between words gracefully.
