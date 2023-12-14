7a12d82972c157612df96f0630a9abd2c61239b8
Aman-Islam-2003 <amanislam019@gmail.com>
2023-12-14 23:53:15 +0500

    Create Task4.js

---
+++ Task4.js
@@ -0,0 +1,1 @@
+//Question: Implement a function called duplicateCount that takes a string as input and returns the count of characters that appear more than once. Ignore case sensitivity. For example, given the input "aabbcA", the function should return 2 because 'a' and 'b' both appear more than once.
